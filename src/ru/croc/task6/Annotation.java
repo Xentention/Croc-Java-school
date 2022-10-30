c7895ee84b9988e1dcfd5b7d0f20e5f466f07a5b
Xentention <xentimoshenko@gmail.com>
2022-10-30 14:05:07 +0300

    A bit of refactoring: added checkIfContains as an Annotation public method instead of a Task6 static one

--- src/ru/croc/task6/Annotation.java
+++ src/ru/croc/task6/Annotation.java
@@ -1,5 +1,7 @@
 package ru.croc.task6;
 
+import static java.lang.Math.pow;
+
 public class Annotation implements Movable {
     protected Figure figure;
     protected String annotatedFigure;
@@ -41,6 +43,31 @@ public class Annotation implements Movable {
         System.out.println(annotatedFigure);
     }
 
+    public boolean checkIfContainsLabel(String label){
+        return this.figure.label.contains(label);
+    }
+
+    public boolean checkIfContainsPoint(double x,
+                                        double y){
+        return switch (this.figure.type) {
+            case CIRCLE -> checkIfCircleContainsPoint(x, y, (Circle) this.figure);
+            case RECTANGLE -> checkIfRectContainsPoint(x, y, (Rectangle) this.figure);
+        };
+    }
+
+    private boolean checkIfCircleContainsPoint(double x,
+                                               double y,
+                                               Circle circle){
+        return pow(x-circle.x, 2) + pow(y-circle.y, 2) <= pow(circle.radius, 2);
+    }
+
+    private boolean checkIfRectContainsPoint(double x,
+                                             double y,
+                                             Rectangle rect){
+        // используется экранная система координат
+        return (rect.LeftBottom[0] <= x && rect.RightTop[0] >= x) &&
+                (rect.RightTop[1] <= y && rect.LeftBottom[1] >= y);
+    }
 
     private void toString(Circle circle){
         this.annotatedFigure = "C (" + circle.x + ", " + circle.y + "), "
--- src/ru/croc/task6/Task6.java
+++ src/ru/croc/task6/Task6.java
@@ -1,7 +1,6 @@
 package ru.croc.task6;
 
 import java.util.ArrayList;
-import static java.lang.Math.pow;
 
 public class Task6 {
     public static void main(String[] args) {
@@ -19,25 +18,29 @@ public class Task6 {
             annotations[i].printAnnotation();
         }
 
+        // перемещаем некоторые объекты
         try {
             annotations[findIndexByLabel("Moo", annotations)].move(3, 3);
+            System.out.println("Annotation that contains 'Moo' was moved by (3, 3)");
         } catch (Exception ObjectNotFound){
             System.out.println("For the label 'Moo' " + ObjectNotFound.getMessage());
         }
 
         try {
             annotations[findIndexByPoint(0, 0, annotations)].move(-3, 10);
+            System.out.println("Annotation that contains (0, 0) was moved by (-3, 10)");
         } catch (Exception ObjectNotFound){
-            System.out.println(ObjectNotFound.getMessage() + " that contains a (10, 10) point.");
+            System.out.println(ObjectNotFound.getMessage() + " that contains a (0, 0) point.");
         }
 
         try {
             annotations[findIndexByPoint(33, 47, annotations)].move(13, 0);
+            System.out.println("Annotation that contains (33, 47) was moved by (13, 0)");
         } catch (Exception ObjectNotFound){
             System.out.println(ObjectNotFound.getMessage() + " that contains a (14, 17) point.");
         }
 
-
+        // выводим обновленный массив
         System.out.println("Updated annotations:");
         for (Annotation annotation
             : annotations) {
@@ -49,7 +52,7 @@ public class Task6 {
     public static int findIndexByLabel(String label,
                                        Annotation... annotations) throws Exception {
         for (int i = 0; i < annotations.length; ++i) {
-            if(annotations[i].figure.label.contains(label)) return i;
+            if(annotations[i].checkIfContainsLabel(label)) return i;
         }
         throw new Exception("Nothing was found");
     }
@@ -58,26 +61,8 @@ public class Task6 {
                                        double y,
                                        Annotation... annotations) throws Exception {
         for (int i = 0; i < annotations.length; ++i)  {
-            boolean havefound = switch (annotations[i].figure.type) {
-                case CIRCLE -> checkIfCircleContainsPoint(x, y, (Circle) annotations[i].figure);
-                case RECTANGLE -> checkIfRectContainsPoint(x, y, (Rectangle) annotations[i].figure);
-            };
-            if(havefound) return i;
+            if(annotations[i].checkIfContainsPoint(x, y)) return i;
         }
         throw new Exception("Nothing was found");
-    }
-
-    private static boolean checkIfCircleContainsPoint(double x,
-                                                      double y,
-                                                      Circle circle){
-        return pow(x-circle.x, 2) + pow(y-circle.y, 2) <= pow(circle.radius, 2);
-    }
-
-    private static boolean checkIfRectContainsPoint(double x,
-                                                    double y,
-                                                    Rectangle rect){
-        // используется экранная система координат
-        return (rect.LeftBottom[0] <= x && rect.RightTop[0] >= x) &&
-                (rect.RightTop[1] <= y && rect.LeftBottom[1] >= y);
     }
 }
