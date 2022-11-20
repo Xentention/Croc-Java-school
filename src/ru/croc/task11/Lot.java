acdcf07acd7bd4c3cecab4c227c60777fe07e959
Xentention <xentimoshenko@gmail.com>
2022-11-20 13:55:31 +0300

    fixed (task 11):     - made it closer to what was initially required     - currentPrice and current buyer are now volatile     - Lot doesn't extend Thread anymore

--- src/ru/croc/task11/Lot.java
+++ src/ru/croc/task11/Lot.java
@@ -1,54 +1,32 @@
 package ru.croc.task11;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
-import static java.time.LocalDateTime.now;
-
-public class Lot extends Thread{
-    private int currentPrice = 0;
-    private int step = 1;
-    private String currentBuyer = "";
-    private LocalDateTime currentTime;
-    private LocalDateTime timeOfSale;
+public class Lot{
+    private volatile int currentPrice = 0;
+    private volatile String currentBuyer = "";
+    private final LocalDateTime endOfAuction;
 
     public Lot(int startingPrice,
-               int step) {
+               LocalDateTime endOfAuction) {
         this.currentPrice = startingPrice;
-        this.step = step;
-        this.currentTime = now();
-    }
-
-
-    @Override
-    // торги за лот
-    public void run() {
-        // вообще должно быть 10 минут, но время уменьшено в целях наглядности
-        while (now().isBefore(currentTime.plusSeconds(10))){
-            // пустое тело таймера
-        }
-        this.interrupt();
-        timeOfSale = now();
-        System.out.println(showWinner());
-
+        this.endOfAuction = endOfAuction;
     }
 
     // задается новая ставка
     public synchronized void getNewBid(int newPrice,
                                        String buyerName) {
-        if (newPrice - currentPrice >= step) {
+        if (LocalDateTime.now().isBefore(endOfAuction)
+                && newPrice > currentPrice) {
             this.currentPrice = newPrice;
             this.currentBuyer = buyerName;
-            this.currentTime = now();
         }
     }
 
-    public String showWinner(){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        if(!currentBuyer.isEmpty())
-            return "Победитель: " + currentBuyer + "\nВремя покупки: " + timeOfSale.format(formatter)
-                    + "\nЦена продажи: " + currentPrice;
-        else
+    public String showWinner() {
+        if (LocalDateTime.now().isBefore(endOfAuction))
             return "Торги не завершены";
+        else
+            return "Победитель: " + currentBuyer;
     }
 }
